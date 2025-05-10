c06dce2fafeee74444382e448544543afb138a9f
Ahmed Sayed <161836894+AhmeedSayeed@users.noreply.github.com>
2025-05-10 17:30:46 +0300

    Update d.sql

--- Requirements Queries/d.sql
+++ Requirements Queries/d.sql
@@ -1,7 +1,7 @@
-select top 1 v.VID, v.VENDOR_NAME, v.VENDOR_EMAIL, v.[ADDRESS], v.VENDOR_PHONE, COUNT(r.RENT_DATE) as "Number of Rents"
+select top 1 with ties v.VID, v.VENDOR_NAME, v.VENDOR_EMAIL, v.[ADDRESS], v.VENDOR_PHONE, COUNT(*) as "Number of Rents"
 from VENDOR v, GAME g, RENTALS r
 where v.VID = g.VID
 	and g.GID = r.GID
-	and r.RENT_DATE >= DATEADD(MONTH, -1, GETDATE())
+	and r.RENT_DATE >= DATEADD(MONTH, -1, CONVERT(DATE, GETDATE()))
 group by v.VID, v.VENDOR_NAME, v.VENDOR_EMAIL, v.[ADDRESS], v.VENDOR_PHONE
-order by COUNT(r.RENT_DATE)
+order by COUNT(*);
