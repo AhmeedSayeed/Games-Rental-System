a2f471c302842ca9f72c3cbc5d08d639e38cdd32
Ahmed Sayed <161836894+AhmeedSayeed@users.noreply.github.com>
2025-05-10 18:33:16 +0300

    Update e.sql

--- Requirements Queries/e.sql
+++ Requirements Queries/e.sql
@@ -3,4 +3,4 @@ from VENDOR v join GAME g
 on v.VID = g.VID left outer join RENTALS r
 on g.GID = r.GID
 group by v.VID, v.VENDOR_NAME, v.VENDOR_EMAIL, v.[ADDRESS], v.VENDOR_PHONE
-having COUNT(r.RENT_DATE) = 0;
+having COUNT(r.GID) = 0;
